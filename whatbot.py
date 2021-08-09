02f8100b154dc97ad6c3b899067f2364c1226c98
cr-trojan23 <pandusrinu1604@gmail.com>
2021-08-09 16:08:52 +0530

    Optimized code and increased speed

--- whatbot.py
+++ whatbot.py
@@ -6,8 +6,8 @@ firefox_driver.get("https://web.whatsapp.com")
 
 input()
 text_box = firefox_driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
-for i in range(6):
-    msg = "Test msg " + str(i)
+for i in range(2001):
+    msg = "enter spam text here"
     text_box.send_keys(msg)
     text_box.send_keys(Keys.ENTER)
-    #time.sleep(2)s
+    #time.sleep(2)
