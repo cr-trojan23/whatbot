781d1c4043ab3c34e0b215cf16bcce4ea8530b55
cr-trojan23 <pandusrinu1604@gmail.com>
2021-09-10 01:15:51 +0530

    Improved Readability

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-This is a spamming bot
+<h1 align="center">WHATBOT</h1>
+
+<p align="center"> whatbot is designed to bulk message ppl. enjoy.</p>
+
+---
+Whatsapp has recently started to ban the accounts which send bulk messages very frequently.
+use at your own risk.
+---
+
+## Installation
+
+```console
+git clone https://github.com/cr-trojan23/whatbot.git
+cd whatbot
+pip install requirements.txt #for windows
+pip3 install requirements.txt #for macos, linux
+python whatbot.py #for windows
+python3 whatbot.py #for macos, linux
+```
+---
+
+Wait for the browser window to open and load whatsapp web.
+Scan the QR Code with your phone.
+Select the chat on whatsapp web to whom you want to send messages to.
+Back in the terminal/cmd, enter the message to be sent and the number of times it should be sent.
+
+---
+
+## License
+Distributed under the MIT license. See LICENSE for more information.
--- whatbot.py
+++ whatbot.py
@@ -1,13 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-firefox_driver = webdriver.Firefox(executable_path="/home/kiaria/Documents/github_projects/whatbot/geckodriver")
+from sys import platform as _platform
+
+# check the operating system
+gecko = ""
+if _platform == "linux" or _platform == "linux2":
+    gecko = "./geckos/geckodriver_linux"
+elif _platform == "darwin":
+    gecko = "./geckos/geckodriver_mac"
+elif _platform == "win32":
+    gecko = "./geckos/geckodriver_win32.exe"
+elif _platform == "win64":
+    gecko = "./geckos/geckodriver_win64.exe"
+
+# create a new instance of the Firefox driver
+firefox_driver = webdriver.Firefox(executable_path=gecko)
+
+# open whatsapp web
 firefox_driver.get("https://web.whatsapp.com")
 
-input()
+# get the xpath of the text box
 text_box = firefox_driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
-for i in range(input("enter number of msgs to be spammed")):
-    msg = input("Enter text to spam")
+
+# message to send
+msg = input("enter message to be spammed\n")
+
+# number of times to send the message
+for i in range(int(input("enter number of msgs to be spammed\n"))):
+
+    # enter the message in the text box
     text_box.send_keys(msg)
+
+    # press enter
     text_box.send_keys(Keys.ENTER)
-    #time.sleep(2)
