2ca363ffb3a3bf40c155ade64dc00d7c3b1027e6
cr-trojan23 <pandusrinu1604@gmail.com>
2021-09-10 16:22:08 +0530

    Fix: Corrected xpath of input text box

--- whatbot.py
+++ whatbot.py
@@ -19,11 +19,12 @@ firefox_driver = webdriver.Firefox(executable_path=gecko)
 # open whatsapp web
 firefox_driver.get("https://web.whatsapp.com")
 
-# get the xpath of the text box
-text_box = firefox_driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
-
 # message to send
 msg = input("enter message to be spammed\n")
+# get the xpath of the text box
+
+text_box = firefox_driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[1]/div/div[2]')
+
 
 # number of times to send the message
 for i in range(int(input("enter number of msgs to be spammed\n"))):
